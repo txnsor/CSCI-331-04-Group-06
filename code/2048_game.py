bb8aae95981bdf6a9265c91df1428036bd33aae0
Marc <pentexb@gmail.com>
2025-10-28 16:25:44 -0400

    optimizations for moving

--- code/2048_game.py
+++ code/2048_game.py
@@ -27,25 +27,7 @@ class Game:
     
     # helper functions
 
-    def __merge(self, dir : DIRECTION):
-        """
-        NOTE: default behavior is to merge LEFT, but any
-        other directional input will use reverse and transpose
-        to get the grid into a form that is equivalent to moving
-        LEFT.
-        """
-
-        match dir:
-            case DIRECTION.RIGHT:
-                # if RIGHT, reverse each row
-                self.grid = np.fliplr(self.grid)
-            case DIRECTION.UP:
-                # if UP, transpose
-                self.grid = self.grid.T
-            case DIRECTION.DOWN:
-                # if DOWN, transpose and then reverse
-                self.grid = np.fliplr(self.grid.T)
-
+    def __merge(self):
         # left merge
         for row in self.grid:
             for i in range(len(row) - 1):
@@ -54,17 +36,19 @@ class Game:
                     row[i] += row[i + 1]
                     row[i + 1] = 0
 
-        # undo rearrangements
-        match dir:
-            case DIRECTION.RIGHT: self.grid = np.fliplr(self.grid)
-            case DIRECTION.UP: self.grid = self.grid.T
-            case DIRECTION.DOWN: self.grid = np.fliplr(self.grid).T
+    def __move_tiles(self):
+        # move to left of arr
+        self.grid = np.array([
+            np.concatenate([row[row != 0], np.zeros(np.sum(row == 0))])
+            for row in self.grid
+        ])
 
-    def __move_tiles(self, dir : DIRECTION):
-        """
-        Compress the grid in one direction.
-        """
+    """
+    Make a 2048 move in a given direction
+    """
+    def move(self, dir : DIRECTION):
 
+    # change any direction into a LEFT move
         match dir:
             case DIRECTION.RIGHT:
                 # if RIGHT, reverse each row
@@ -76,26 +60,16 @@ class Game:
                 # if DOWN, transpose and then reverse
                 self.grid = np.fliplr(self.grid.T)
 
-        # move to left of arr
-        self.grid = np.array([
-            np.concatenate([row[row != 0], np.zeros(np.sum(row == 0))])
-            for row in self.grid
-        ])
+        # MOVE -> MERGE -> MOVE guarantees things work properly
+        self.__move_tiles()
+        self.__merge()
+        self.__move_tiles()
 
         # undo rearrangements
         match dir:
             case DIRECTION.RIGHT: self.grid = np.fliplr(self.grid)
             case DIRECTION.UP: self.grid = self.grid.T
             case DIRECTION.DOWN: self.grid = np.fliplr(self.grid).T
-
-    """
-    Make a 2048 move in a given direction
-    """
-    def move(self, dir : DIRECTION):
-        # MOVE -> MERGE -> MOVE guarantees things work properly
-        self.__move_tiles(dir)
-        self.__merge(dir)
-        self.__move_tiles(dir)
 
 
 # testing
