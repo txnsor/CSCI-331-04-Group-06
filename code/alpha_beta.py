7839a108e38744fe6d7a07316203ef89dbad20db
Cole Stowell <cole@stowell.pro>
2025-11-17 12:01:37 -0500

    feat: alpha beta pruning

--- code/alpha_beta.py
+++ code/alpha_beta.py
@@ -1,26 +1,28 @@
 from game import Game, DIRECTION
 
 # min
-def alpha_beta_rng(game: Game, depth: int) -> tuple[float, tuple[int, int] | None]:
+def alpha_beta_rng(game: Game, depth: int, prune: bool, alpha: float, beta: float) -> tuple[float, tuple[int, int] | None]:
     if depth == 0 or game.is_over():
         return game.score(), None
 
     best = float('inf')
-    total = 0
     pos = None
     for p in game.get_free_spaces():
         g = game.copy()
         g.grid[p] = 2
 
-        v, _ = alpha_beta_user(g, depth-1)
-        total += v
+        v, _ = alpha_beta_user(g, depth-1, prune, alpha, beta)
         if v < best:
             best = v
             pos = p
+            beta = min(beta, v)
+
+        if beta <= alpha and prune:
+            break
     return best, pos
 
 # max
-def alpha_beta_user(game: Game, depth: int) -> tuple[float, DIRECTION | None]:
+def alpha_beta_user(game: Game, depth: int, prune: bool, alpha: float, beta: float) -> tuple[float, DIRECTION | None]:
     if depth == 0 or game.is_over():
         return game.score(), None
 
@@ -28,11 +30,15 @@ def alpha_beta_user(game: Game, depth: int) -> tuple[float, DIRECTION | None]:
     dir = None
     for d in [DIRECTION.DOWN, DIRECTION.UP, DIRECTION.LEFT, DIRECTION.RIGHT]:
         g = game.copy()
-        g.move(d)
-        v, _ = alpha_beta_rng(g, depth-1)
+        if not g.move(d):
+            continue
+        v, _ = alpha_beta_rng(g, depth-1, prune, alpha, beta)
         if v > best:
             best = v
             dir = d
+            alpha = max(alpha, v)
+        if beta <= alpha and prune:
+            break
 
     return best, dir
 
@@ -41,7 +47,8 @@ def main():
 
     while True:
         game.random_place_tile()
-        _, dir = alpha_beta_user(game, 7)
+        _, dir = alpha_beta_user(game, 7, True, float('-inf'), float('inf'))
+
         if dir == None:
             break
         game.move(dir)
